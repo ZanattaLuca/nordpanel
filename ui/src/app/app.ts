02e2fa281360d526e80bf27be04ecc4b208e7bb0
Luca Zanatta <lucazanatta90@gmail.com>
2025-12-05 23:55:28 +0100

    chore: create a toolbar

--- ui/src/app/app.ts
+++ ui/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
